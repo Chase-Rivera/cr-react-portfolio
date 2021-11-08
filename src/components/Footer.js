17b8a69bbad43606d8e49c0796b89b8718ee05a0
Chase Rivera <achaserivera@gmail.com>
2021-11-07 20:57:55 -0800

    built out footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import { Github, Linkedin } from "react-bootstrap-icons"
+
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+                <a href="https://github.com/Chase-Rivera">
+                    <Github/>
+                </a>
+                <a href="https://www.linkedin.com/in/alexander-rivera-9a5562216/">
+                    <Linkedin/>
+                </a>
+            </div>
+        </footer>
+    )
+};
