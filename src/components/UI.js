e7026dacd47b659039e3ac646e3fc0c16354e542
Chase Rivera <achaserivera@gmail.com>
2021-11-07 20:47:12 -0800

    built out starting user interface

---
+++ src/components/UI.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { Portfolio } from "./pages/Portfolio"
+import { Contact } from "./pages/Contact"
+import { Resume } from "./pages/Resume"
+import AboutMe from "./pages/AboutMe"
+import Header from "./Header"
+import Footer from "./Footer"
+
+export default function UI() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+    const [value, setValue] = useState(0);
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe/>;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio/>;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume/>;
+        }
+        return <Contact/>;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} value={value} setValue={setValue}/>
+            {renderPage()}
+            <Footer/>
+        </>
+
+    )
+}
