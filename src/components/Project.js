567999a05c087fa082d705f198aba5ee5803b3b0
Chase Rivera <achaserivera@gmail.com>
2021-11-08 11:57:13 -0800

    built out project comp

---
+++ src/components/Project.js
@@ -0,0 +1,27 @@
+import { Card, Col  } from "react-bootstrap";
+import {Github } from "react-bootstrap-icons";
+
+export default function Project(props) {
+
+    return (
+        <>
+            <Col>
+                <Card className="big-dark text-white pcard">
+                    <div className={props.img}>
+                        <Card.Img/>
+                    </div>
+                    <Card.ImgOverlay>
+                        <div className="projectlabel">
+                            <Card.Title><a href={props.deployed}>{props.title}</a></Card.Title>
+                            <Card.Text>{props.tech}<br/>
+                            <div className="github">
+                                <a href={props.github}><Github/></a>
+                            </div>
+                            </Card.Text>
+                        </div>
+                    </Card.ImgOverlay>
+                </Card>
+            </Col>
+        </>
+    )
+};
