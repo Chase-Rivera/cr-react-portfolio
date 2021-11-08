909e6c131a8b6e96273017598fdf484400710ef8
Chase Rivera <achaserivera@gmail.com>
2021-11-08 13:19:14 -0800

    built resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,11 @@
+export function Resume() {
+
+    return (
+        <> 
+            <div className="banner">
+                <h1>Resume coming soon!</h1>
+            </div>
+            <div className="pageview"></div>
+        </>
+    )
+};
